21a8f885323151096eb93b4226fb2674714efc97
LmOpe <lawalmuhammedope@gmail.com>
2024-06-22 20:25:41 +0100

    Update profile serialilzer, include username field

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -8,17 +8,21 @@ from .mixins import UpdateSerializerMixin
 class ProfileSerializer(UpdateSerializerMixin, serializers.ModelSerializer):
     dob = serializers.DateField(input_formats=['%d-%m-%Y'])
     age = serializers.SerializerMethodField(read_only=True)
+    username = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Profile
         fields = ['user', 'avatar', 'nutritional_goal', 'sex', 'dob', 'weight',\
-             'height', 'activity_level', 'weight_unit', 'height_unit', 'age']
+             'height', 'activity_level', 'weight_unit', 'height_unit', 'age', 'username']
 
     def get_age(self, obj):
         dob = obj.dob
         today = datetime.today()
         age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
         return age
+  
+    def get_username(self, obj):
+        return obj.user.username
 
 class NotificationPreferencesSerializer(UpdateSerializerMixin, serializers.ModelSerializer):
     class Meta:
