3623f1acd2bb7a65014fa0f4e545378f3207edf6
LmOpe <lawalmuhammedope@gmail.com>
2024-07-31 20:23:12 +0100

    Configure email backend

--- fudhouse/settings_prod.py
+++ fudhouse/settings_prod.py
@@ -9,8 +9,12 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=False)
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=['https://fdh-inky.vercel.app/', 'https://fdh-lmos-projects.vercel.app/', 'localhost', '127.0.0.1', '.vercel.app', '.now.sh'])
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
