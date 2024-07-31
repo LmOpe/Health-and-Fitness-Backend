bcd700d0f2a104054cee29d84b0490ed1c047c67
LmOpe <lawalmuhammedope@gmail.com>
2024-07-31 07:44:53 +0100

    Add psycopg2-binary

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    settings_module = 'fudhouse.settings_prod'  if os.getenv('DJANGO_ENV') == 'production' else "fudhouse.settings_local"
+    settings_module = 'fudhouse.settings_prod'  if os.getenv('DEBUG')  else "fudhouse.settings_local"
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
     try:
         from django.core.management import execute_from_command_line
